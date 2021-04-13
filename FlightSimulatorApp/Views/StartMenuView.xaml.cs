e69108ae804baa3d583d76254913145fb35757a0
ILAN BITAN <ilan.bitan@live.biu.ac.il>
2021-04-14 00:12:40 +0300

    Added fixed instrcuctions and ClassDiagram1 (UML)

--- FlightSimulatorApp/Views/StartMenuView.xaml.cs
+++ FlightSimulatorApp/Views/StartMenuView.xaml.cs
@@ -88,13 +88,17 @@ namespace FlightSimulatorApp.Views
 
         private void ShowInstructionsPopUp(object sender, RoutedEventArgs e)
         {
-            CheckBox cb = new CheckBox();
-
-            MessageBoxResult result = MessageBox.Show("Welcome to Ilan Bitan instructions Ⓒ", "Instuctions", MessageBoxButton.OK);
-            while (cb.IsChecked == null)
-            {
-                continue;
-            }
+            MessageBoxResult result = MessageBox.Show("Welcome to our instructions\n" +
+                "1) If you want to start the program:\n\t" +
+                "Click on 'upload train CSV button' and choose the correct file\n\t" +
+                "then you can choose Offline/Online mode.\n" +
+                "2) Offline mode: run the program without the FlightGear.\n" +
+                "3) Online mode: run the program with the FlightGear.\n" +
+                "4) If you want to speedup or slowup the program you can slide the " +
+                "'Play Speed' slider to change the current speed.\n" +
+                "5) If you want to see the values of the 'Linear Regression Graph' you need to roll the mouse pulley " +
+                "beside the left area of the graphs and choose your own attribute.\n" +
+                "♡ you from Ilan, Noam, Hanna and Asaf", "Instuctions", MessageBoxButton.OK);
         }
     }
 
